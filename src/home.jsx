50ae395b9c47f3baa332e35f4b2d47c9a6a36c43
Carlos Astorga Martínez <carlos.astorgax@gmail.com>
2020-03-16 16:49:01 -0300

    terminado el header

--- src/home.jsx
+++ src/home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/navbar.jsx'
 import Footer from './components/footer.jsx'
 import Welcome from './components/home/welcome.jsx'
@@ -9,36 +9,43 @@ import CallToAction from './components/home/calltoaction.jsx'
 
 const Home = props => {
 
-    return (
+    const [state, setState] = useState({
+        gallery: [
+            {
+                id:1,
+                image: `https://picsum.photos/200/300`,
+                title: `First Slide`,
+                description: `This is the description for the first slide...`
+            },
+            {
+                id:2,
+                image: `https://picsum.photos/200/300`,
+                title: `Second Slide`,
+                description: `This is the description for the first slide...`
+            },
+            {
+                id:3,
+                image: `https://picsum.photos/200/300`,
+                title: `Third Slide`,
+                description: `This is the description for the first slide...`
+            }
+        ]
+    })
 
+    return (
         <>
             <Navbar />
-
-            <Header/>
-                
+            <Header galleryData={state.gallery}/>
             <div className="container">
-
                 <Welcome />
-
                 <h2>Portfolio Heading</h2>
-
-                <Portfolio/>
-
-                <ModernBusiness/>
-
+                <Portfolio />
+                <ModernBusiness />
                 <hr />
-
-            <CallToAction/>
-
+                <CallToAction />
             </div>
-
             <Footer />
-
         </>
-
     )
-
 }
-
 export default Home;
-
